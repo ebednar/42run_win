2298b6fba3e708a73c7ca917f606f94bff2fc197
unknown <lexetl@yandex.ru>
2020-12-14 10:04:29 +0300

    skybox started

---
+++ 42run_win/include/skybox.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class Skybox
+{
+public:
+	unsigned char* text_data;
+	unsigned int	vbo;
+	unsigned int	vao;
+	unsigned int	texture;
+	int				shader_id;
+public:
+};
---
+++ 42run_win/src/skybox.cpp
@@ -0,0 +1,6 @@
+#include "skybox.h"
+#include "glad.h"
+#include "stb_image.h"
+#include "shader.h"
+#include <iostream>
+#include <vector>
